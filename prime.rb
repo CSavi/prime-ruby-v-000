3f8e316f28dbc6fd74ae980bc29e55b8d408bb41
Carrie Savinell <carrielane89@gmail.com>
2018-05-08 14:21:03 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,20 @@
-# Add  code here!
+
+
+def prime?(n)
+  i = 2
+  if n == 2 
+    return true 
+  end 
+  while i < n 
+    if n % i == 0 
+    return false 
+  i = i + 1 
+  end 
+
+end 
+  
+  
+  
+
+   
+    
